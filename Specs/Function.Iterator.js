fc46a441698d65055eafc7f50027ff986ba4080f
Olmo Maldonado <olmo.maldonado@gmail.com>
2010-10-03 18:38:47 -0500

    Updated Spec with new specs. All pass.

--- Specs/Function.Iterator.js
+++ Specs/Function.Iterator.js
@@ -2,22 +2,83 @@
 describe('Function.Iterator.js', function(){
 	
 	it('accepts any number of functions', function(){
+		var iter = new Function.Iterator();
+		expect(iter.items.length).toBe(0);
 		
+		iter = new Function.Iterator(function(){}, function(){});
+		expect(iter.items.length).toBe(2);
+	});
+	
+	it('provides StopIteration Error object', function(){
+		new StopIteration();
 	});
 	
 	it('throws StopIteration when there is no next function', function(){
+		var iter = new Function.Iterator();
+		expect(function(){ iter.next(); }).toThrow(StopIteration);
 		
+		var i = 0;
+		iter = new Function.Iterator(function(){ i++ }, function(){ i++ });
+		
+		expect(function(){
+			 // Being careful not to run forever before testing
+			for (var j = 0; j < 5; j++) iter.next();
+		}).toThrow(StopIteration);
+		expect(i).toBe(2);
 	});
 	
 	it('can push more functions to iterate', function(){
+		var iter = new Function.Iterator();
+		iter.push(function fn(){});
 		
+		expect(iter.items.length).toBe(1);
+		expect(iter.items[0]).toBe(fn);
+		
+		iter = new Function.Iterator(function a(){});
+		iter.push(function b(){}, function c(){});
+		
+		expect(iter.items.length).toBe(3);
+		expect(iter.items[0]).toBe(a);
+		expect(iter.items[1]).toBe(b);
+		expect(iter.items[2]).toBe(c);
+		expect(iter.items[3]).toBeNull();
+	});
+	
+	it('will return the generated value, or undefined', function(){
+		var iter = new Function.Iterator(function(){
+			return 'first';
+		}, function(){
+			return 'second';		
+		}, function(){
+		});
+		
+		expect(iter.next()).toBe('first');
+		expect(iter.next()).toBe('second');
+		expect(iter.next()).toBe(undefined);
 	});
 	
 	it('can iterate through each of the functions', function(){
+		var iter = new Function.Iterator(function(){
+			return 'first';
+		}, function(){
+			return 'second';		
+		});
 		
+		var results = [], count = 0, lastIndex = -1;
+		iter.each(function(current, i){
+			results.push(current);
+			count++;
+			lastIndex = i;
+		});
+		
+		expect(results.length).toBe(2);
+		expect(results[0]).toBe('first');
+		expect(results[1]).toBe('second');
+		expect(count).toBe(2);
+		expect(lastIndex).toBe(1);
 	});
 	
-	it('can skip, or continue, to the next iteration', function(){
+	xit('can skip, or continue, to the next iteration', function(){
 		
 	});
 	
